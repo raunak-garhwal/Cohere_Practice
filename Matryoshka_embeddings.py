5875e69abb525eb826855a6ababfbfbd37027b49
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-04-30 18:27:38 +0530

    files added

--- Matryoshka_embeddings.py
+++ Matryoshka_embeddings.py
@@ -2,7 +2,7 @@ import cohere
 
 co = cohere.ClientV2(api_key="B0n3BcGthprXNg5s4z6BmHdsD2hnH1iLcb5eeWnp")
 
-texts = ["hello"]
+texts = ["hello","56"]
 
 response = co.embed(
     model="embed-v4.0",
@@ -12,5 +12,6 @@ response = co.embed(
     embedding_types=["float"]
 )
 
-print(response.embeddings.float)  # returns a vector that is 1024 dimensions
-print(len(response.embeddings.float[0]))
+print(response.embeddings.float)   # returns a list of embeddings for each text in the input list
+print(len(response.embeddings.float))   # returns the number of texts in the input list
+print(len(response.embeddings.float[0]))   # returns a vector that is 1024 dimensions
